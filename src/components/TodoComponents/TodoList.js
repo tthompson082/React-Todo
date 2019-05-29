ccaccfe9b5ec8b3e63cd86db9048091f974e7419
Tyler Thompson <tylerthompson@Tylers-MacBook-Pro.local>
2019-05-29 16:24:49 -0400

    app is passing array to todolist and todolist is using map to send each object in the array to todo to appear on screen

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,19 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from 'react';
+
+import Todo from './Todo'
+
+class ToDoList extends React.Component {
+    render() {
+        return (
+            <div>
+                {this.props.toDoArray.map(toDo =>(
+                    <Todo toDoProp={toDo} />
+                ))}
+            </div>
+        );
+    }
+}
+
+export default ToDoList;
